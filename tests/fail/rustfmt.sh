748f19fd2dc8ce4fc3a6b998843cf6d9eccf118f
Dominic <git@msrd0.de>
2021-03-09 23:01:46 +0100

    add GitHub Actions

--- tests/fail/rustfmt.sh
+++ tests/fail/rustfmt.sh
@@ -15,7 +15,7 @@ esac
 
 return=0
 find "$(dirname "$0")" -name '*.rs' -type f | while read file; do
-	$rustfmt --config-path "$(dirname "$0")/../../../rustfmt.toml" "$@" "$file" || return=1
+	$rustfmt --config-path "$(dirname "$0")/../../rustfmt.toml" "$@" "$file" || return=1
 done
 
 exit $return

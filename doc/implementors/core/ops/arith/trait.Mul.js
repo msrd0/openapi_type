bb7ae3590f675d980795998e08d5d75977f3a3f9
msrd0 <msrd0@users.noreply.github.com>
2021-03-21 16:47:50 +0000

    GitHub Pages for 72b9d7bf55be323dfa9b4ee6401c157f7f7ff282

--- doc/implementors/core/ops/arith/trait.Mul.js
+++ doc/implementors/core/ops/arith/trait.Mul.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["chrono"] = [{"text":"impl Mul&lt;i32&gt; for Duration","synthetic":false,"types":[]}];
 implementors["time"] = [{"text":"impl Mul&lt;i32&gt; for Duration","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/core/ops/arith/trait.Neg.js
+++ doc/implementors/core/ops/arith/trait.Neg.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["chrono"] = [{"text":"impl Neg for Duration","synthetic":false,"types":[]}];
 implementors["time"] = [{"text":"impl Neg for Duration","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

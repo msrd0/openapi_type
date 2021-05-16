54a87f0fd68fffa64e9a87f17c08be90d524309f
msrd0 <msrd0@users.noreply.github.com>
2021-05-16 15:09:04 +0000

    Revert "$ref-erence all non-trivial oneOf objects (#12)"
    This reverts commit 351ee8ce5217440d6489b84088bb31f26f76247d.

--- src/private.rs
+++ src/private.rs
@@ -12,37 +12,22 @@ fn add_dependencies(dependencies: &mut Dependencies, other: &mut Dependencies) {
 	}
 }
 
-/// Add the schema to the dependencies and always return a reference. The schema's dependencies
-/// are always added to the dependencies.
-pub fn reference<T, F>(dependencies: &mut Dependencies, mut schema: OpenapiSchema, fallback_ref_name: F) -> ReferenceOr<T>
-where
-	F: FnOnce() -> String
-{
-	add_dependencies(dependencies, &mut schema.dependencies);
-
-	let ref_name = schema
-		.name
-		.as_deref()
-		.map(|name| name.replace(|c: char| !c.is_alphanumeric(), "_"))
-		.unwrap_or_else(fallback_ref_name);
-	let reference = format!("#/components/schemas/{}", ref_name);
-
-	dependencies.insert(ref_name, schema);
-	ReferenceOr::Reference { reference }
-}
-
-/// Inline the schema if it is unnamed, otherwise return a reference to it and add it to the
-/// dependencies. The schema's dependencies are always added to the dependencies.
 pub fn inline_if_unnamed(
 	dependencies: &mut Dependencies,
 	mut schema: OpenapiSchema,
 	doc: Option<&'static str>
 ) -> ReferenceOr<Schema> {
-	match &schema.name {
-		Some(_) => reference(dependencies, schema, || unreachable!()),
+	add_dependencies(dependencies, &mut schema.dependencies);
+	match schema.name.as_ref() {
+		Some(schema_name) => {
+			let ref_name = schema_name.replace(|c: char| !c.is_alphanumeric(), "_");
+			let mut reference = "#/components/schemas/".to_string();
+			reference.push_str(&ref_name);
+			dependencies.insert(ref_name, schema);
+			ReferenceOr::Reference { reference }
+		},
 
 		None => {
-			add_dependencies(dependencies, &mut schema.dependencies);
 			let mut schema = schema.into_schema();
 			if let Some(doc) = doc {
 				schema.schema_data.description = Some(doc.to_string());

dd86b78e1c70b5b857918c20c93f261e6627a7d6
msrd0 <msrd0@users.noreply.github.com>
2022-01-01 12:14:51 +0000

    GitHub Pages for ba2a0a98e509df32a3738d9c84bf088b3992f657

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,9 +1,8 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["chrono"] = {"name":"","dirs":[{"name":"format","files":["mod.rs","parse.rs","parsed.rs","scan.rs","strftime.rs"]},{"name":"naive","files":["date.rs","datetime.rs","internals.rs","isoweek.rs","time.rs"]},{"name":"offset","files":["fixed.rs","mod.rs","utc.rs"]}],"files":["date.rs","datetime.rs","div.rs","lib.rs","oldtime.rs","round.rs"]};
-sourcesIndex["dtoa"] = {"name":"","files":["diyfp.rs","dtoa.rs","lib.rs"]};
 sourcesIndex["hashbrown"] = {"name":"","dirs":[{"name":"external_trait_impls","files":["mod.rs"]},{"name":"raw","files":["alloc.rs","bitmask.rs","mod.rs","sse2.rs"]}],"files":["lib.rs","macros.rs","map.rs","scopeguard.rs","set.rs"]};
 sourcesIndex["indexmap"] = {"name":"","dirs":[{"name":"map","dirs":[{"name":"core","files":["raw.rs"]}],"files":["core.rs"]}],"files":["equivalent.rs","lib.rs","macros.rs","map.rs","mutable_keys.rs","serde.rs","serde_seq.rs","set.rs","util.rs"]};
-sourcesIndex["itoa"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["itoa"] = {"name":"","files":["lib.rs","udiv128.rs"]};
 sourcesIndex["linked_hash_map"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["num_integer"] = {"name":"","files":["average.rs","lib.rs","roots.rs"]};
 sourcesIndex["num_traits"] = {"name":"","dirs":[{"name":"ops","files":["checked.rs","inv.rs","mod.rs","mul_add.rs","overflowing.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","sign.rs"]};
